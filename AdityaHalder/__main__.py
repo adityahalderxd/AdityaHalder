47446cb120780af0bc8f13829a5f38a36485c358
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-19 08:31:25 +0530

    Update __main__.py

--- AdityaHalder/__main__.py
+++ AdityaHalder/__main__.py
@@ -10,25 +10,48 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from pytgcalls.exceptions import NoActiveGroupCall
 
 from AdityaHalder import config
-from AdityaHalder import LOGGER, robot, userx, HELPABLE
 from AdityaHalder.plugins import ALL_MODULES
 from AdityaHalder.modules.core.sudo import SUDOERS
 from AdityaHalder.modules.helpers.filters import command
+from AdityaHalder.modules.core.clients import app, ass, bot
 from AdityaHalder.utilities.inline import paginate_modules
 
 loop = asyncio.get_event_loop()
 
 
 async def init():
-    if (
-        not config.STRING_SESSION
-        and not config.SESSION_STRING
-    ):
+    LOGGER("AdityaHalder").info("Starting Bot Client")
+    if (not bot):
         LOGGER("AdityaHalder").error(
-            "No Sessions Vars Defined!.. Exiting Process."
+            "Bot Client Start Failed!.. Exiting Process."
         )
         return
-    await robot.start()
+    await bot.start()
+    LOGGER("AdityaHalder").info("Bot Client Successfully Started")
+    LOGGER("AdityaHalder").info("Starting User Bot Client")
+    if (not config.STRING_SESSION):
+        LOGGER("AdityaHalder").error(
+            "String Session Not Found!.. Exiting Process."
+        )
+        return
+    if (not app):
+        LOGGER("AdityaHalder").error(
+            "User Bot Client Not Found!.. Exiting Process."
+        )
+        return
+    await app.start()
+    LOGGER("AdityaHalder").info("User Bot Started Successfully")
+    LOGGER("AdityaHalder").info("Checking Assistant Client ...")
+    if (ass):
+        LOGGER("AdityaHalder").info(
+            "Assistant Client Found, Starting Assistant Client"
+        )
+        await ass.start()
+        LOGGER("AdityaHalder").info("Assistant Client Started")
+    else:
+     LOGGER("AdityaHalder").info(
+            "Assistant Client Not Found!.."
+        )
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module("AdityaHalder.plugins." + all_module)
         if (
@@ -45,7 +68,8 @@ async def init():
                     ] = imported_module
     LOGGER("AdityaHalder.plugins").info(f">> Successfully imported: {all_module}.py")
     LOGGER("AdityaHalder.plugins").info("All Modules Imported")
-    await userx.start()
+    
+    
     LOGGER("AdityaHalder").info("Aditya Halder Started Successfully")
     await idle()
 
