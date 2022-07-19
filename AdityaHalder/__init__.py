f85117519f3a14102b59f31643cccc9d732785b5
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-19 11:19:54 +0530

    Update __init__.py

--- AdityaHalder/__init__.py
+++ AdityaHalder/__init__.py
@@ -3,6 +3,7 @@ from AdityaHalder.modules.core.dirs import dirs
 from AdityaHalder.modules.core.github import git
 from AdityaHalder.modules.core.sudo import sudo
 from AdityaHalder.modules.core.clients import *
+from AdityaHalder.modules.core.info import *
 
 from .logging import LOGGER
 
@@ -27,3 +28,17 @@ sudo()
 app = app
 ass = ass
 bot = bot
+
+# Clients Information
+
+app_fnm = app_fnm
+app_unm = app_unm
+app_uid = app_uid
+
+ass_fnm = ass_fnm
+ass_unm = ass_unm
+ass_uid = ass_uid
+
+bot_fnm = bot_fnm
+bot_unm = bot_unm
+bot_uid = bot_uid
