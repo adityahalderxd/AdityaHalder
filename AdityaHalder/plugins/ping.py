a3153941807bbaff7ff98165207175dffebb5c9d
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-19 10:43:59 +0530

    Create ping.py

---
+++ AdityaHalder/plugins/ping.py
@@ -0,0 +1,21 @@
+import requests
+from datetime import datetime
+from pyrogram import filters, Client
+from AdityaHalder.utilities.misc import SUDOERS
+# ping checker
+
+@Client.on_message(filters.command(["ping"], ["/", ".", "!"]) & SUDOERS)
+async def ping(Client, message):
+    start = datetime.now()
+    loda = await message.reply_text("**Checking ...**")
+    end = datetime.now()
+    mp = (end - start).microseconds / 1000
+    await loda.edit_text(f"**🤖 Aditya's Ping\n»** `{mp} ms`")
+
+
+__MODULE__ = "Pɪɴɢ"
+__HELP__ = f"""
+**🥀 Cʜᴇᴄᴋ Yᴏᴜʀ Gᴇɴɪᴜs UsᴇʀBᴏᴛ Pɪɴɢ.**
+
+`.ping` - **Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ Tᴏ Cʜᴇᴄᴋ**
+"""
