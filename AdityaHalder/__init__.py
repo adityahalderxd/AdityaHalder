3b5d94037675099558883bed8f8866541a003bf0
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-19 09:19:04 +0530

    Update __init__.py

--- AdityaHalder/__init__.py
+++ AdityaHalder/__init__.py
@@ -6,12 +6,23 @@ from AdityaHalder.modules.core.sudo import sudo
 from .logging import LOGGER
 
 
+# Helpers
+
 HELPABLE = {}
 MOD_LOAD = []
 MOD_NOLOAD = []
 
 
+# Checker
+
 dirs()
 git()
 dbb()
 sudo()
+
+
+# Clients
+
+app = app
+ass = ass
+bot = bot
