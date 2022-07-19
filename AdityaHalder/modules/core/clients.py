b8b981f46bcdbb90c4d5618c2c57ad80949e1f7f
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-19 08:32:37 +0530

    Update and rename client.py to clients.py

---
+++ AdityaHalder/modules/core/clients.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from AdityaHalder.config import *
+from pyrogram import Client
+from ...logging import LOGGER
+
+
+if STRING_SESSION:
+    app = Client(
+        session_name=STRING_SESSION,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=dict(root="AdityaHalder.plugins")
+)
+else:
+    app = None
+
+if SESSION_STRING:
+    ass = Client(
+        session_name=SESSION_STRING,
+        api_id=API_ID,
+        api_hash=API_HASH,
+)
+else:
+    ass = None
+
+if BOT_TOKEN:
+    bot = Client(
+        "AdityaHalder",
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+        bot_token=config.BOT_TOKEN,
+)
+else:
+    bot = None
